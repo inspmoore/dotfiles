476624e21d8c64f04930afc156c159520edbc5b1
lucas <morawski.lukasz@outlook.com>
2025-02-18 17:21:34 +0100

    remove old stuff

--- .config/tmux/mode-trigger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "copy-mode" ] || [ "$1" == "view-mode" ]; then
-  # Set the background color to your preferred color when copy mode is entered
-  tmux setw -g status-bg 'colour203'
-else
-  # Reset the background color to the original color when copy mode is exited
-  tmux setw -g status-bg 'colour188'
-fi
